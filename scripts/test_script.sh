85749d5946bdef8563efe02d464a69441b7fb53e
mikanikos <andrea.piccione@epfl.ch>
2020-03-20 17:18:19 +0100

    Modify script, try to fix weird bug on connection handler

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 cd ..
+cd monitor
 go build
+cd ..
 cd validator
 go build
 cd ..
@@ -9,10 +11,10 @@ cd ..
 echo "Starting validators
 "
 
-./validator/validator -config="config_1.yaml" -address="127.0.0.1:8080" &
-./validator/validator -config="config_2.yaml" -address="127.0.0.1:8081" &
-./validator/validator -config="config_3.yaml" -address="127.0.0.1:8082" &
-./validator/validator -config="config_4.yaml" -address="127.0.0.1:8083" &
+./validator/validator -config="config_1.yaml" -address="127.0.0.1:21990" &
+./validator/validator -config="config_2.yaml" -address="127.0.0.1:21991" &
+./validator/validator -config="config_3.yaml" -address="127.0.0.1:21992" &
+./validator/validator -config="config_4.yaml" -address="127.0.0.1:21993" &
 
 sleep 2
 
@@ -20,9 +22,9 @@ echo "
 Starting monitor
 "
 
-./Fork-Accountability -processes="127.0.0.1:8080,127.0.0.1:8081,127.0.0.1:8082,127.0.0.1:8083" -firstDecisionRound=3 -secondDecisionRound=4 -waitTimeout=5
+./monitor/monitor -processes="127.0.0.1:21990,127.0.0.1:21991,127.0.0.1:21992,127.0.0.1:21993" -firstDecisionRound=3 -secondDecisionRound=4 -waitTimeout=5 &
 
-sleep 1
+sleep 7
 
 pkill -f validator
-pkill -f Fork-Accountability
+pkill -f monitor
