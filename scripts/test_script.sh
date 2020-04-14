d15d4c2194c07fb89e5ad55e21bcdb0f4380f0f2
mikanikos <andrea.piccione@epfl.ch>
2020-04-14 17:27:13 +0200

    Edit test script

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -15,10 +15,10 @@ cd ../..
 echo "Starting validators
 "
 
-./cmd/validator/validator -config="/cmd/validator/_config/config_1.yaml" -delay=1 &
-./cmd/validator/validator -config="/cmd/validator/_config/config_2.yaml" -delay=5 &
-./cmd/validator/validator -config="/cmd/validator/_config/config_3.yaml" -delay=1 &
-./cmd/validator/validator -config="/cmd/validator/_config/config_4.yaml" -delay=5 &
+./cmd/validator/validator -config="cmd/validator/_config/config_1.yaml" -delay=1 &
+./cmd/validator/validator -config="cmd/validator/_config/config_2.yaml" -delay=5 &
+./cmd/validator/validator -config="cmd/validator/_config/config_3.yaml" -delay=1 &
+./cmd/validator/validator -config="cmd/validator/_config/config_4.yaml" -delay=5 &
 
 # give some time to validators to start listening
 sleep 1
@@ -27,7 +27,7 @@ echo "
 Starting monitor
 "
 
-./cmd/monitor/monitor -config="/cmd/monitor/_config/config.yaml" -report="true" &
+./cmd/monitor/monitor -config="/cmd/monitor/_config/config.yaml" -report="cmd/monitor/report.out" &
 
 # give some time for communication and running the algorithm
 sleep 15
