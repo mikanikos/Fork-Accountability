ecfdf705883ed1c663ba98cd3d6c2ecc4a3adc70
mikanikos <andrea.piccione@epfl.ch>
2020-04-14 14:35:15 +0200

    COnvert monitor to server

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 cd ..
+
+go build ./...
+
 cd cmd/validator
 go build
 
@@ -14,13 +17,18 @@ numValidators=4
 echo "Starting validators
 "
 
-for i in $(seq 1 $numValidators);
-do
-  ./cmd/validator/validator -config="/cmd/validator/_config/config_$i.yaml" &
-done
+# for i in $(seq 1 $numValidators);
+# do
+#   ./cmd/validator/validator -config="/cmd/validator/_config/config_$i.yaml" &
+# done
+
+./cmd/validator/validator -config="/cmd/validator/_config/config_1.yaml" -delay=1 &
+./cmd/validator/validator -config="/cmd/validator/_config/config_2.yaml" -delay=5 &
+./cmd/validator/validator -config="/cmd/validator/_config/config_3.yaml" -delay=1 &
+./cmd/validator/validator -config="/cmd/validator/_config/config_4.yaml" -delay=5 &
 
 # give some time to validators to start listening
-sleep 2
+sleep 1
 
 echo "
 Starting monitor
@@ -29,7 +37,7 @@ Starting monitor
 ./cmd/monitor/monitor -config="/cmd/monitor/_config/config.yaml" &
 
 # give some time for communication and running the algorithm
-sleep 5
+sleep 15
 
 pkill -f validator
 pkill -f monitor
